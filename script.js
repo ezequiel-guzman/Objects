ea45628a0b516d2809951669d76c954cbdcfe40b
EZEQUIEL GUZMAN <57122751+ezequiel-guzman@users.noreply.github.com>
2022-10-07 07:31:20 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var user = {
+  userName: "Izzy",
+  password: 1234,
+}
+
+var database = [user];
+
+var newsfeed = 
+[
+{
+  username: "john",
+  timeline: 111,
+},
+{
+  username: "phill",
+  timeline: 222,
+},
+{
+  username: "mark",
+  timeline: 333,
+},]
